1a77e94995c66cd667d12614fa59bcc120dcf11e
Adithya Krishna <vineetha.kadithya2001@gmail.com>
2021-07-28 23:19:15 +0530

    Modified data into JSON, user can now click and drag

--- test.js
+++ test.js
@@ -5,17 +5,37 @@ const height = robot.getScreenSize().height;
 const width = robot.getScreenSize().width;
 
 const server = net.createServer((socket) => {
-	var ndx, ndy, temp;
+	var ndx, ndy,temp;
 	var mouse;
 	console.log('client connected');
 
 	socket.on('data', (data) => {
-		temp = data.toString().split(',');
-		ndx = parseFloat(temp[0]);
-		ndy = parseFloat(temp[1]);
+		var temp = data.toString();
+		temp = temp.substring(temp.search("{"),temp.search("}")+1);
+		console.log(temp);
+		try {
+			const obj = JSON.parse(temp);
+			ndx = obj.dx;
+			ndy = obj.dy;
+			mouse = robot.getMousePos();
+			switch (obj.action) {
+				case "click":
+					robot.mouseClick();
+					break;
+				case "move":
+					robot.moveMouse(width * ndx, height * ndy);
+					break;
+				case "drag":
+					robot.mouseToggle("down");
+					robot.dragMouse(width * ndx, height * ndy);
+					robot.mouseToggle("up");
+					break;
+			}
+		} catch (error) {
+			console.log(error)
+		}
 		
-		mouse = robot.getMousePos();
-		robot.moveMouse(width * ndx, height * ndy);
+		
 	});
 });
 
